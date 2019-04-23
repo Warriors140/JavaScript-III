f47c6d00cb60fff1e4c5b5cc33880f918e00f213
Randy Warrington <warrington.randy@outlook.com>
2019-04-23 16:40:58 -0400

    explained 4 principles of this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,12 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When a function is in a global scope, the value of "this" inside of the function becomes
+     the window object.
+* 2. When a function is called by a previous dot, the object before that dot is now "this".
+* 3. When a constructor function is used, "this" is refered to the specific instance of the
+     object that is created and returned by the constructor function.
+* 4. When JavaScript's "call" or "apply" method is used, "this" is explicitly defined.
 *
 * write out a code example of each explanation above
 */
